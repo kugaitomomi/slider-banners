c50df5ab2ac23480dc2654aaf8e148d9507742a0
kugaitomomi <tottomo7@gmail.com>
2022-11-09 16:32:44 +0900

    adjust: carousel and audio

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -10,17 +10,19 @@ import { faPause } from "@fortawesome/free-solid-svg-icons";
 function Player() {
   const [play, { stop, pause }] = useSound(Sound);
   return (
-    <div className={classes.Audioarea}>
-      <p>BGM</p>
-      <button onClick={() => play()}>
-        <FontAwesomeIcon icon={faPlay} />
-      </button>
-      <button onClick={() => pause()}>
-        <FontAwesomeIcon icon={faPause} />
-      </button>
-      <button onClick={() => stop()}>
-        <FontAwesomeIcon icon={faStop} />
-      </button>
+    <div className={classes.AudioContainer}>
+      <div className={classes.Audioarea}>
+        <p>再生ボタンをクリックすると音楽が再生されます。</p>
+        <button className={classes.AudioButton} onClick={() => play()}>
+          <FontAwesomeIcon icon={faPlay} />
+        </button>
+        <button className={classes.AudioButton} onClick={() => pause()}>
+          <FontAwesomeIcon icon={faPause} />
+        </button>
+        <button className={classes.AudioButton} onClick={() => stop()}>
+          <FontAwesomeIcon icon={faStop} />
+        </button>
+      </div>
     </div>
   );
 }
